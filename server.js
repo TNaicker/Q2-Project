1d0b48803b97b75dfaf466ade2ee9e77e3d8939e
Tegan Naicker <Tegan.Naicker@gmail.com>
2017-08-28 16:02:59 -0400

    Add heroku env config

---
+++ server.js
@@ -0,0 +1,33 @@
+const express       = require('express');
+const app           = express();
+const APP_MODE      = process.env.APP_MODE;
+const dbConfig      = require('./knexfile')[APP_MODE];
+const knex          = require('knex')(dbConfig);
+const bodyParser    = require('body-parser');
+const cookieSession = require('cookie-session');
+var x = 10;
+
+const PORT = 8000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+//hi
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(cookieSession({
+  name: 'user_cookie',
+  keys: ['socure1', 'socure2']
+}))
+
+const sessions = require('./routes/sessions.js');
+
+app.use(sessions);
+
+app.use((req, res) => {
+  res.sendStatus(418);
+})
+
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+})
